9be7770ddabab7636c93dfd149dd8cf8543b330e
Kamrul Huda Sattari Saad <skamrul2680@gmail.com>
2022-08-20 00:35:44 +0600

    added header and navbar

---
+++ src/Components/Home/Header.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div
+            className="hero min-h-[calc(100vh-68px)]"
+            style={{ backgroundImage: "url(assets/1.jpg)" }}
+        >
+            <div className="hero-overlay bg-opacity-60" />
+            <div className="hero-content text-center text-neutral-content">
+                <div className="max-w-md">
+                    <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
+                    <p className="mb-5">
+                        Welcome to the world of learning in a better way. A journey where only obstacle you find is yourself. A better learning platform for your everyday studies.
+                    </p>
+                    <button className="btn btn-primary">Get Started</button>
+                </div>
+            </div>
+        </div>
+
+    );
+};
+
+export default Header;
